f71cb97bc94ed64f685c4b870f0cd9553f63afd8
Kevin Lee <keverino@Kevins-MBP.home>
2020-11-02 19:17:44 -0800

    change cron job back to 3min

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const {
 const scrapeSites = require('./src/lib/scrapeSites');
 
 const job = new CronJob({
-  //cronTime: '0 */1 * * * *',
   cronTime: '0 */3 * * * *',
   onTick: async function () {
     await console.log('\n***You will see this message every 3 minutes ***\n');
